ba12e3e37d3af420e84f926eee6112627df4002e
sapsalgae10 <ssapssalgae0902@gmail.com>
2026-03-21 21:57:03 +0900

    Update design

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,15 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+// next.config.js
+module.exports = {
+  images: {
+    domains: ["images.ctfassets.net"], // 사용할 이미지 도메인을 허용합니다.
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net", // 프로토콜과 호스트명을 명시하여 외부 이미지 경로를 허용합니다.
+      },
+    ],
+  },
+};
